b0dfa6fd173898cde5d2abdf09ffe61df123ccaf
Lorenzo Costa <costa86@zoho.com>
2021-05-11 11:25:33 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 export function getDistanceCoordinatesInKm(lat1,lon1,lat2,lon2){
+  const p = Math.PI/180;
   const radius = 6.371;
-  const latitudeOne = lat1 * Math.PI/180; 
-  const latitudeTwo = lat2 * Math.PI/180;
-  const deltaLatitude = (lat2-lat1) * Math.PI/180;
-  const deltaLongitude = (lon2-lon1) * Math.PI/180;
+  const latitudeOne = lat1 * p; 
+  const latitudeTwo = lat2 * p;
+  const deltaLatitude = (lat2-lat1) * p;
+  const deltaLongitude = (lon2-lon1) * p;
   
   const a = Math.sin(deltaLatitude/2) * Math.sin(deltaLatitude/2) +
             Math.cos(latitudeOne) * Math.cos(latitudeTwo) *
             Math.sin(deltaLongitude/2) * Math.sin(deltaLongitude/2);
+  
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+  
   return radius * c;
 }
 
